98fb58542985e6bc3e900ce9004ef3b1fb003e0f
Caio Laganá Fernandes <caiolagana@gmail.com>
2024-04-23 23:04:50 -0300

    wip word embedding tutorial

--- TensorFlow/nw4.py
+++ TensorFlow/nw4.py
@@ -9,8 +9,23 @@ import re
 import shutil
 import string
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from keras.layers import TextVectorization
 
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
-from tensorflow.keras.layers import TextVectorization
 
+
+dataset_dir = os.path.join('.', 'aclImdb')
+print(os.listdir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+print(os.listdir(train_dir))
+
+batch_size = 1024
+seed = 123
+train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train', batch_size=batch_size, validation_split=0.2,
+    subset='training', seed=seed)
+val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train', batch_size=batch_size, validation_split=0.2,
+    subset='validation', seed=seed)
